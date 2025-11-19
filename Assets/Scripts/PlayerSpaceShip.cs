f2112aeb02bbbefa7ecd5cfe2b627bba6d5d9ca5
Javier Figueroa <12277277+2NA@users.noreply.github.com>
2025-11-19 21:44:29 +0100

    Clase 1

---
+++ Assets/Scripts/PlayerSpaceShip.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerSpaceShip : MonoBehaviour
+{
+    [SerializeField] float maxSpeed = 100f;
+    [SerializeField] float acceleration = 300f;
+
+    [SerializeField] InputActionReference move;
+    [SerializeField] InputActionReference shoot;
+
+    private void OnEnable()
+    {
+        move.action.Enable();
+        shoot.action.Enable();
+
+        move.action.started += OnMove;
+        move.action.performed += OnMove;
+        move.action.canceled += OnMove;
+
+        shoot.action.started += OnShoot;
+    }
+
+    Vector2 currentVelocity = Vector2.zero;
+    void Update()
+    {
+        currentVelocity += rawMove * acceleration * Time.deltaTime;
+
+        float linearVelocity = currentVelocity.magnitude;
+        linearVelocity = Mathf.Clamp(linearVelocity, 0, maxSpeed);
+        currentVelocity = currentVelocity.normalized * linearVelocity;
+        
+        transform.Translate(currentVelocity * Time.deltaTime);
+    }
+
+    private void OnDisable()
+    {
+        move.action.Disable();
+        shoot.action.Disable();
+        
+        move.action.started -= OnMove;
+        move.action.performed -= OnMove;
+        move.action.canceled -= OnMove;
+
+        shoot.action.started -= OnShoot;
+    }
+
+    Vector2 rawMove;
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        rawMove = context.ReadValue<Vector2>();
+    }
+
+    private void OnShoot(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
